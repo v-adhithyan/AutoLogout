194782ce59001cd82c801458a3538339d096cb8f
v.adhithyan <aavispeaks@gmail.com>
2016-02-26 10:00:57 +0530

    16,48,128 bit icons added and related changes

--- scripts/logout.js
+++ scripts/logout.js
@@ -1,5 +1,6 @@
 var timer = window.setInterval(logout, 600000);
 var i = 0;
 function logout(){
-	alert("10 minutes over.. Close tab now");
+	alert("10 minutes over.. Tab will be closed now");
+	window.close();
 }
