ac486e12dda673e2857b34d70104be8f94ab2545
Adhithyan Vijayakumar <aavispeaks@gmail.com>
2016-02-26 23:06:50 +0530

    timer placement changed

--- scripts/logout.js
+++ scripts/logout.js
@@ -18,8 +18,9 @@ function logout(){
 	if(quoraLogoutCheck(logoutForm.action)){
 		alert("You will be logged out now.");
 		document.getElementsByTagName("form")[0].submit();
-		stopTimer();
 	}else{
 		console.log("no logout");
-	}	
+	}
+	
+	stopTimer();
 }
