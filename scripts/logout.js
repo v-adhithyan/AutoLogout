8e20d129b29608a03a87b1f4f571bc601d115f03
v.adhithyan <aavispeaks@gmail.com>
2016-02-29 16:34:15 +0530

    twitter logout functionality added

--- scripts/logout.js
+++ scripts/logout.js
@@ -1,7 +1,13 @@
+//for production
+//var timer = window.setInterval(fb, 600000);
+//for testing
 var timer = window.setInterval(logout, 3000);
 
-function quoraLogoutCheck(action){	
+function logoutCheck(action){	
 	if(action.indexOf("logout") > 0){
+		return true;
+	}
+	else if(action.indexOf("signout") > 0){
 		return true;
 	}
 	return false;
@@ -15,12 +21,34 @@ function logout(){
 	var currentPage = window.location.href;
 	var logoutForm = document.getElementsByTagName("form")[0];
 
-	if(quoraLogoutCheck(logoutForm.action)){
-		alert("You will be logged out now.");
-		document.getElementsByTagName("form")[0].submit();
-	}else{
-		console.log("no logout");
+	console.log("current page:"+currentPage);
+
+	if(currentPage.indexOf("twitter") > 0){
+		
+		var signoutButton = document.getElementById("signout-button");
+		
+		if(logoutCheck(signoutButton.id)){
+			alert("You will be logged out now.");
+			signoutButton.click();		
+		}
+
 	}
+	else if(currentPage.indexOf("quora") > 0){
+		
+		var logoutForm = document.getElementsByTagName("form")[0];
+		
+		if(logoutCheck(logoutForm.action)){
+			alert("You will be logged out now.");
+			 document.getElementsByTagName("form")[0].submit();
+		}
+
+	}
+	else{
+		
+		console.log("no logout");
+		alert("close tab now.");
 	
+	}
+
 	stopTimer();
 }
