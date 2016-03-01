7d3118d68e695c36868b222acdaae6677370cc2c
v.adhithyan <aavispeaks@gmail.com>
2016-03-01 09:28:17 +0530

    facebook logout functionality added

--- scripts/logout.js
+++ scripts/logout.js
@@ -1,5 +1,5 @@
 //for production
-//var timer = window.setInterval(fb, 600000);
+//var timer = window.setInterval(logout, 600000);
 //for testing
 var timer = window.setInterval(logout, 3000);
 
@@ -17,6 +17,14 @@ function stopTimer(){
 	clearTimeout(timer);
 }
 
+function isUndefined(obj){
+	if(typeof obj === 'undefined')
+		return true;
+	else if(obj == null)
+		return true;
+	return false;
+}
+
 function logout(){
 	var currentPage = window.location.href;
 	var logoutForm = document.getElementsByTagName("form")[0];
@@ -27,27 +35,51 @@ function logout(){
 		
 		var signoutButton = document.getElementById("signout-button");
 		
-		if(logoutCheck(signoutButton.id)){
+		if(!isUndefined(signoutButton) && logoutCheck(signoutButton.id)){
 			alert("You will be logged out now.");
 			signoutButton.click();		
 		}
 
 	}
 	else if(currentPage.indexOf("quora") > 0){
-		
+			
 		var logoutForm = document.getElementsByTagName("form")[0];
 		
-		if(logoutCheck(logoutForm.action)){
+		if(!isUndefined(logoutForm) && logoutCheck(logoutForm.action)){
 			alert("You will be logged out now.");
-			 document.getElementsByTagName("form")[0].submit();
+			document.getElementsByTagName("form")[0].submit();
 		}
 
 	}
-	else{
-		
-		console.log("no logout");
-		alert("close tab now.");
-	
+	else if(currentPage.indexOf("facebook") > 0){
+		var insertedNodes = [];
+		var observer = new WebKitMutationObserver(function(mutations){
+			console.log("dom change");
+			mutations.forEach(function(mutation){
+				for(var j=0; j<mutation.addedNodes.length; j++)
+				insertedNodes.push(mutation.addedNodes[j]);
+				console.log("j"+insertedNodes[j].id);
+			})
+
+			var logoutForm = document.getElementsByClassName("_w0d")[0];
+			if(!isUndefined(logoutForm)){
+				observer.disconnect();
+				alert("You will be logged out now.");
+				logoutForm.submit();
+			}
+		});
+
+		observer.observe(document, {
+			attributes: true,
+			childList: true,
+			characterData: true,
+			subtree: true
+		});
+
+		var logoutMenu = document.getElementById("userNavigationLabel");	
+		if(!isUndefined(logoutMenu))
+			logoutMenu.click();
+
 	}
 
 	stopTimer();
